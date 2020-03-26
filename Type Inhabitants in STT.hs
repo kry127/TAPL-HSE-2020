fe03b5f84154a562fa1a15a46c666dc330a727db
kry127 <kry127@yandex.ru>
2020-03-27 00:05:52 +0300

    Added algorithm Ben-Yells

--- Type Inhabitants in STT.hs
+++ Type Inhabitants in STT.hs
@@ -22,27 +22,37 @@ uncurry2List :: Type -> (Symb,[Type])
 uncurry2List (TVar name) = (name, [])
 uncurry2List (t1 :-> t2) = (t1:) <$> uncurry2List t2
 
-uncurry2RevList  :: Type -> (Symb,[Type])
-uncurry2RevList (TVar name) = (name, [])
-uncurry2RevList (t1 :-> t2) = (++[t1]) <$> uncurry2RevList t2
+uncurry2RevList :: Type -> (Symb,[Type])
+uncurry2RevList t = go [] (uncurry2List t)
+ where
+  go buf (name, x:xs) = go (x:buf) (name, xs)
+  go buf (name, []  ) = (name, buf)
 
--- строит для каждой метапеременной список всех подходящих замен
-envolutionSubalg :: Ctx -> TNF -> [TNF]
-envolutionSubalg ctx (Meta t) =
-    (target, typelist) =  uncurry2RevList t
-	
+-- раскрытие метапеременной в структурированную схему (в контексте)
+envelopMeta :: Ctx -> Type -> [TNF]
+envelopMeta ctx (TVar name) = do
+  -- перебираем все возможные доступные типы из контектса (с номером)
+  (i, t) <- zip [0..] ctx
+  -- ищем, кто заканчивается на ту же переменную
+  ctx' <- [c | let (name', c) = uncurry2List t, name == name']
+  return $ TNF [] i (Meta <$> ctx')
+envelopMeta ctx t@(a :-> b) = do
+  let (alpha, typelist) = uncurry2RevList t
+  -- расширяем контекст найденными типами
+  (TNF [] idx tails) <- envelopMeta (typelist ++ ctx) (TVar alpha)
+  -- добавляем найденные типы к структурному описанию
+  return $ TNF typelist idx tails
+
 
 unMeta :: Ctx -> TNF -> [TNF]
-unMeta zetas (Meta t) = let
-        (target, typelist) = uncurry2RevList t
-    in (\x -> TNF zetas x []) <$> go zetas target typelist
-  where
-    go zetas target (h:t) = do
-      id <- elemIndices h zetas
-      return $ id: go zetas target t
-	
-	
+unMeta ctx (Meta t) = envelopMeta ctx t -- для метапеременных применяем субалгоритм раскрытия
+unMeta ctx (TNF abs i ts) = do -- для NF-схем делаем рекурсивные вызовы
+  -- добавляем абстрактор к текущему контексту и вызываем алгоритм подстановки метапеременных рекурсивно
+  ts' <- sequenceA $ (unMeta (abs ++ ctx)) <$> ts -- аппл. функтор, семантика: каждый с каждым
+  return $ TNF abs i ts' -- обновляем все возможные Бёмовы хвостики
 
+unMeta0 :: Type -> [TNF]
+unMeta0 t = unMeta [] (Meta t)
 
 --- tests
 tArr = TVar "a" :-> TVar "a"
